a3627b4e6fdbbdd070a78d66deb338f509c7fa74
gneelwarna <gauravneelwarna7@gmail.com>
2017-09-18 23:16:02 +0530

    Added Flask support. Dump entries on webpage

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-import argparse, os
-import subprocess
+from flask import Flask
 from preprocessor import preprocessor
 from parser import airodump_parser
 
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['get'])
+def home():
+    #Read airodump-ng output csv file.
+    header, entries = airodump_parser('/root/1-01.csv')
+    return 'Welcome to WiFiRyu!' + str(header) + ' - ' + str(entries)
+
+
 def main():
     print 'Welcome to WiFiRyu -->'
-
     # Prepare output directory in /tmp to for airodump-ng
     preprocessor().prepare_output_dir()
-
     #Create monitor mode interface.
     #Code to be added.
 
@@ -16,14 +24,9 @@ def main():
     #Code to be added.
 
     #Read airodump-ng output csv file.
-    header, entries = airodump_parser('/root/1-01.csv')
-    print header
-    print entries
-
-
-def dump():
-    print '--- Inside dump'
+    #header, entries = airodump_parser('/root/1-01.csv')
 
 
 if __name__ == '__main__':
+    app.run(debug=True)
     main()
