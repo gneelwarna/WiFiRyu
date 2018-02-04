78e45ad94d675f261013cffcb641a32430d48653
gneelwarna <gauravneelwarna7@gmail.com>
2018-02-05 01:50:32 +0530

    Basic API for airmon and airodump added. Authentication, error handling and fail-over cases remaining.

--- main.py
+++ main.py
@@ -1,32 +1,191 @@
-from flask import Flask
-from preprocessor import preprocessor
-from parser import airodump_parser
-
+from flask import Flask, jsonify, request, json, abort, Response
+from preprocess import preprocessor
+from utils import airodump_json_parser
+import os
+from pathlib2 import Path
+import subprocess
+import signal
+import shlex
 
 app = Flask(__name__)
+monitor_interfaces = []
 
 
-@app.route('/', methods=['get'])
+@app.route('/')
 def home():
-    #Read airodump-ng output csv file.
-    header, entries = airodump_parser('/root/1-01.csv')
-    return 'Welcome to WiFiRyu!' + str(header) + ' - ' + str(entries)
+    print "Welcome to WiFiRyu!"
 
 
-def main():
-    print 'Welcome to WiFiRyu -->'
-    # Prepare output directory in /tmp to for airodump-ng
-    preprocessor().prepare_output_dir()
-    #Create monitor mode interface.
-    #Code to be added.
+@app.route('/login')
+def login():
+    args = ""
+    for i in request.args:
+        args += args
+    return str(args)
+    # return app.send_static_file('login.html')
 
-    #Start airodump-ng in subprocess.
-    #Code to be added.
 
-    #Read airodump-ng output csv file.
-    #header, entries = airodump_parser('/root/1-01.csv')
+@app.route('/airmon/list')
+def airmon_list():
+    output = (subprocess.check_output('airmon-ng').split('\n'))
+    interfaces = []
+    for line in output:
+        if line != "" and line[0] != " ":
+            print line.split("\t")
+            if line.split("\t")[1] != "Interface":
+                temp_dict = {}
+                temp_dict['interface'] = line.split()[1]
+                temp_dict['driver'] = line.split()[2]
+                interfaces.append(temp_dict)
+    print '[-] Interfaces = ' + str(interfaces)
+    data = { 'status' : 'ok', 'interfaces':interfaces}
+    resp = jsonify(data)
+    resp.status_code = 200
+    return resp
+
+
+@app.route('/airmon/interface', methods=['PUT'])
+def airmon_interface():
+    print request.args['operation'] + ' ' + request.args['interface']
+    output = subprocess.check_output(['airmon-ng ' +
+                                      request.args['operation'] +
+                                      ' ' +
+                                      request.args['interface']],
+                                     shell=True)
+    if request.args['operation'].lower() == 'start':
+        output = output.split('mac80211 monitor mode vif enabled')[1] \
+            .split('\n')[0].split(' on ')[1].split(']')[1][:-1]
+        if not monitor_interfaces.__contains__(output):
+            monitor_interfaces.append(output)
+    else:
+        output = output.split('mac80211 station mode vif enabled')[1] \
+            .split('\n')[0].split(' on ')[1].split(']')[1][:-1]
+        if monitor_interfaces.__contains__(output):
+            monitor_interfaces.remove(output)
+    data = {}
+    data['status'] = 'ok'
+    data['interface'] = output
+    resp = jsonify(data)
+    resp.status_code = 200
+    return resp
+
+
+@app.route('/airmon/check')
+def airmon_check():
+    output = subprocess.check_output(['airmon-ng check'], shell=True)
+    processes = []
+    if output.__contains__('PID Name'):
+        output = output.split('PID Name')[1].split('\n')
+        for process in output:
+            if process != '':
+                processes.append(process.strip().split(' ')[1])
+        print processes
+    data = {}
+    data['status'] = 'ok'
+    data['processes'] = processes
+    resp = jsonify(data)
+    resp.status_code = 200
+    return resp
+
+
+@app.route('/airmon/checkkill')
+def airmon_checkkill():
+    output = subprocess.check_output(['airmon-ng check kill'], shell=True)
+    processes = []
+    if output.__contains__('PID Name'):
+        output = output.split('PID Name')[1].split('\n')
+        for process in output:
+            if process != '':
+                processes.append(process.strip().split(' ')[1])
+        print processes
+    data = {}
+    data['status'] = 'ok'
+    data['processes'] = processes
+    resp = jsonify(data)
+    resp.status_code = 200
+    return resp
+
+
+@app.route('/airmon/moninterface', methods=['GET'])
+def airmon_moninterface():
+    data = {}
+    data['status'] = 'ok'
+    data['interfaces'] = monitor_interfaces
+    resp = jsonify(data)
+    resp.status_code = 200
+    return resp
+
+
+@app.route('/airodump', methods=['GET'])
+def airodump():
+    dump_file = Path('/tmp/ryu/airodump-sample.csv')
+    if dump_file.is_file():
+        return airodump_json_parser('/tmp/ryu/airodump-sample.csv')
+    else:
+        data = {}
+        data['status'] = 'error'
+        data['message'] = 'Airodump output file not found.'
+        resp = jsonify(data)
+        resp.status_code = 200
+        return resp
+
+
+@app.route('/airodump', methods=['PUT'])
+def airodump_op():
+    cmd = 'airodump-ng '
+    request_data = request.get_json()
+    operation = request_data['operation']
+    if operation.lower() == 'start':
+        if request_data['attributes']['gpsd'] == "True":
+            cmd += '--gpsd '
+        if request_data['attributes']['berlin'] != 0:
+            cmd += '--berlin ' + \
+                   str(request_data['attributes']['berlin']) + ' '
+        if request_data['attributes']['band'] != "":
+            cmd += '--band ' + \
+                str(request_data['attributes']['band']) + ' '
+
+        if request_data['attributes']['manufacturer'] == "True":
+            cmd += '--manufacturer '
+        if request_data['attributes']['uptime'] == "True":
+            cmd += '--uptime '
+        if request_data['attributes']['wps'] == "True":
+            cmd += '--wps '
+        if request_data['attributes']['write-interval'] != 0:
+            cmd += '--write-interval ' + \
+                   str(request_data['attributes']['write-interval']) + ' '
+        if request_data['attributes']['encrypt'] != "":
+            cmd += '--encrypt ' + \
+                   str(request_data['attributes']['encrypt']) + ' '
+        if request_data['attributes']['bssid'] != "":
+            cmd += '--bssid ' + \
+                   str(request_data['attributes']['bssid']) + ' '
+        if request_data['attributes']['essid-regex'] != "":
+            cmd += '--essid-regex ' + \
+                   str(request_data['attributes']['essid-regex']) + ' '
+        cmd += '-w /tmp/ryu/airodump '
+        interface = request_data['interface']
+        cmd += interface
+        print 'cmd = ' + cmd
+        # raw_input("What you say, continue?    ")
+        proc = subprocess.Popen(shlex.split(cmd))
+        data = {}
+        data['status'] = 'ok'
+        data['pid'] = proc.pid
+        resp = jsonify(data)
+        resp.status_code = 200
+        return resp
+    else:
+        os.kill(request_data['pid'], signal.SIGKILL)
+        data = {}
+        data['status'] = 'ok'
+        data['pid'] = -1
+        resp = jsonify(data)
+        resp.status_code = 200
+        return resp
 
 
 if __name__ == '__main__':
+    # preproc = preprocessor()
+    # preproc.prepare_output_dir()
     app.run(debug=True)
-    main()
---
+++ utils.py
@@ -0,0 +1,30 @@
+from flask import jsonify
+import json, csv
+
+
+def strip_input(entry):
+    entry = entry.strip('\n').strip('\r').rsplit(',')
+    stripped_entry = []
+    for i in entry:
+        stripped_entry.append(str(i).strip(' '))
+    return stripped_entry
+
+
+def airodump_json_parser(airodump_file):
+    f = open(airodump_file, 'r')
+    f.readline() # Blank line
+
+    header = strip_input(f.readline())
+    json_array = []
+    while True:
+        json_entry = {}
+        line = f.readline()
+        if line in ['\n', '\r\n']:
+            break
+        header_list = map(str, header)
+        line_list = map(str, strip_input(line))
+        entry_item = iter(line_list)
+        for header_item in header_list:
+            json_entry[header_item] = str(entry_item.next())
+        json_array.append(json_entry)
+    return jsonify(json_array)
